f653af0b1ffd9ba972cc1e904a154c28910d62e2
Yar <yaroslav239@gmail.com>
2022-01-08 12:41:05 +0300

    added effects

--- js/script.js
+++ js/script.js
@@ -24,3 +24,14 @@ $(function(){
         ]
       });
 });
+
+
+$('a[href^="#"').on('click', function() {
+
+  let href = $(this).attr('href');
+
+  $('html, body').animate({
+      scrollTop: $(href).offset().top
+  });
+  return false;
+});
